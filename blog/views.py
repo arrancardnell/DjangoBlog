186de70d74b51662dc06e15a476afbd9751b00a5
arrancardnell <arrancardnell@gmail.com>
2016-06-15 19:31:16 +0100

    Added registration.

--- blog/views.py
+++ blog/views.py
@@ -8,7 +8,7 @@ from django.shortcuts import render, get_object_or_404
 from haystack.query import SearchQuerySet
 from taggit.models import Tag
 
-from .forms import EmailPostForm, CommentForm, SearchForm, LoginForm
+from .forms import EmailPostForm, CommentForm, SearchForm, LoginForm, UserRegistrationForm
 from .models import Post
 
 
@@ -135,3 +135,21 @@ def user_login(request):
         form = LoginForm()
 
     return render(request, 'login.html', {'form': form})
+
+
+def register(request):
+    if request.method == 'POST':
+        user_form = UserRegistrationForm(request.POST)
+        if user_form.is_valid():
+            # Create a new user object but avoid saving it yet
+            new_user = user_form.save(commit=False)
+            # Set the chosen password
+            new_user.set_password(user_form.cleaned_data['password'])
+            # Save the User object
+            new_user.save()
+            # Create the user profile
+            # profile = Profile.objects.create(user=new_user)
+            return render(request, 'blog/register_done.html', {'new_user': new_user})
+    else:
+        user_form = UserRegistrationForm()
+    return render(request, 'blog/register.html', {'user_form': user_form})
