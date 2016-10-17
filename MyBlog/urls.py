fe73b129a031f067a3e39ebafc94d27d6ef054a2
arrancardnell <arrancardnell@gmail.com>
2016-10-17 20:55:26 +0100

    Changed colour scheme and logo

--- MyBlog/urls.py
+++ MyBlog/urls.py
@@ -24,8 +24,8 @@ sitemaps = {
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
-    url(r'^blog/', include('django.contrib.auth.urls')),
-    url(r'^blog/', include('blog.urls',
+    url(r'^', include('django.contrib.auth.urls')),
+    url(r'^', include('blog.urls',
                            namespace='blog',
                            app_name='blog')),
     url(r'^sitemap\.xml$', sitemap, {'sitemaps': sitemaps},
